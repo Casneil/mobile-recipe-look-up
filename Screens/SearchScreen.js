cec8d5f0a4b14d055b006ec4102fb67ad3fbf7d8
Casneil Simpson <casneil.simpson@gmail.com>
2020-01-04 21:18:22 +0100

    implementing yelp api

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  const request = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term: query,
+        location: "berlin"
+      }
+    });
+    setData(response.data.businesses);
+  };
+
   return (
     <View>
       <SearchBar
         query={query}
         onQueryChange={newQuery => setQuery(newQuery)}
-        onQuerySumbit={() => console.log("Term submitted")}
+        onQuerySumbit={() => request()}
       />
       <Text>Search Screen</Text>
-      <Text>{query}</Text>
+      <Text>We found {data.length} results</Text>
     </View>
   );
 };
